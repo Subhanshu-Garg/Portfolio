ba811ea04cdddb5ec6dc044b36ec8cd843687694
Subhanshu Garg <subhanshu.garg@freecharge.com>
2024-03-05 14:49:32 +0530

    add animation to intro

--- js/script.js
+++ js/script.js
@@ -17,6 +17,39 @@ animation.forEach((element) => {
     observer.observe(element)
 })
 
+const myName = 'Subhanshu';
+const designation = "Software Developer";
+const introElement = document.querySelector('#intro');
+async function typeOutText(text, element, delay = 100) {
+    introElement.innerHTML = '|'
+    for (const ch of text) {
+        await new Promise(resolve => setTimeout(resolve, delay));
+        element.innerHTML = element.innerText.slice(0, -1)
+        element.innerHTML += `${ch}|`;
+    }
+}
+async function deleteText(element, delay = 100) {
+    const text = element.innerText;
+    for (let i = text.length; i > 0; i--) {
+        await new Promise(resolve => setTimeout(resolve, delay));
+        element.innerText = element.innerText.slice(0, -2) + '|';
+    }
+}
+
+async function typeAnimation() {
+    await typeOutText(myName, introElement, 100);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await deleteText(introElement, 50);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await typeOutText(designation, introElement, 100);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await deleteText(introElement, 50);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    typeAnimation(); 
+}
+
+typeAnimation();
+
 /////////////////carousel//////////////////////////////
 
 const carousel = document.querySelector('#carousel');
