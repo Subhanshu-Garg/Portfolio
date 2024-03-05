7a4f4344d4f586754443da4593d38dbf9e66ae46
Subhanshu Garg <subhanshu.garg@freecharge.com>
2024-03-05 19:38:56 +0530

    enhance the changes

--- js/script.js
+++ js/script.js
@@ -52,34 +52,76 @@ typeAnimation();
 
 /////////////////carousel//////////////////////////////
 
-const carousel = document.querySelector('#carousel');
-const prevButton = document.querySelector('#prev-button');
-const nextButton = document.querySelector('#next-button');
+// const carousel = document.querySelector('#carousel');
+// const prevButton = document.querySelector('#prev-button');
+// const nextButton = document.querySelector('#next-button');
 
-const carouselItems = Array.from(carousel.querySelectorAll('.carousel-item'));
+// const carouselItems = Array.from(carousel.querySelectorAll('.carousel-item'));
 
 
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-    carouselItems.forEach((item, index) => {
-        if (index === currentIndex) {
-        item.style.display = 'block';
-        } else {
-        item.style.display = 'none';
-        }
-    });
-    });
+// prevButton.addEventListener('click', () => {
+//     currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+//     carouselItems.forEach((item, index) => {
+//         if (index === currentIndex) {
+//         item.style.display = 'block';
+//         } else {
+//         item.style.display = 'none';
+//         }
+//     });
+//     });
 
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % carouselItems.length;
-    carouselItems.forEach((item, index) => {
-        if (index === currentIndex) 
-        item.style.display = 'block';
-    });
-});
+// nextButton.addEventListener('click', () => {
+//     currentIndex = (currentIndex + 1) % carouselItems.length;
+//     carouselItems.forEach((item, index) => {
+//         if (index === currentIndex) 
+//         item.style.display = 'block';
+//     });
+// });
 
 const contactForm = document.querySelector('#contactForm');
-
+emailjs.init({
+    publicKey: 'IKxipXtoFXDPv4CtZ'
+});
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    emailjs.send("service_cjr2lbq","template_o518t7e",{
+        to_name: "Subhanshu",
+        from_name: e.target.name.value,
+        message: e.target.message.value,
+        reply_to: e.target.email.value,
+    }).then((response) => {
+        console.log('Email sent successfully:', response);
+        alert('Message sent successfully!');
+    }, (error) => {
+        console.error('Failed to send email:', error);
+        alert('Failed to send message. Please try again later.');
+    }).finally(() => {
+        e.target.reset()
+    });;
 } )
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((button) => {
+    // Mouse Interaction
+    button.addEventListener('mouseover', function() {
+        this.classList.add('hovered');
+    });
+
+    button.addEventListener('mouseout', function() {
+        this.classList.remove('hovered');
+    });
+
+    // Touch Interaction
+    button.addEventListener('touchstart', function(e) {
+        this.classList.add('hovered');
+        e.preventDefault()
+    });
+
+    button.addEventListener('touchend', async function() {
+        await new Promise(resolve => setTimeout(resolve, 100));
+        this.classList.remove('hovered');
+        button.click()
+    });
+})
+
